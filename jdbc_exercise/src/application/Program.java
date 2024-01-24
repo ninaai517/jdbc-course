cebc86bc4f6e753f16b6691ebf215f03196e48ed
ninaai517 <carolina.aizawa.moreira@gmail.com>
2024-01-24 19:38:43 -0300

    Demo - how to recover data from Database using JDBC

--- jdbc_exercise/src/application/Program.java
+++ jdbc_exercise/src/application/Program.java
@@ -1,19 +1,39 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
 
-	public static void main(String[] args) throws SQLException {
-		
+	public static void main(String[] args) {
+
 		Connection conn = DB.getConnection();
-		
-		if(!(conn.isClosed())) {
-			System.out.println("Okay");
+		Statement st = null;
+		ResultSet rs = null;
+
+		try {
+			conn = DB.getConnection();
+
+			st = conn.createStatement();
+			rs = st.executeQuery("select * from department");
+
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + " , " + rs.getString("Name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			DB.closeConnection();
+
 		}
+
 	}
 
 }
